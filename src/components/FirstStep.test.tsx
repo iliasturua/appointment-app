f73e74c0719593ca4b434b6b3bd3a79e3d5bc24b
iliasturua <iliasturua@gmail.com>
2023-05-27 14:11:36 +0400

    feat: add more cases to FirstStep test

--- src/components/FirstStep.test.tsx
+++ src/components/FirstStep.test.tsx
@@ -1,8 +1,7 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import FirstStep from "./FirstStep";
 import { Form } from "antd";
 import userEvent from "@testing-library/user-event";
-import { act } from "react-dom/test-utils";
 
 describe("FirstStep", () => {
   beforeAll(() => {
@@ -16,7 +15,11 @@ describe("FirstStep", () => {
       };
   });
 
-  it("is rendering FirstStep component", async () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("displays an error message when required fields are not filled", async () => {
     const appointment = {
       firstName: "",
       lastName: "",
@@ -39,40 +42,149 @@ describe("FirstStep", () => {
       </Form>
     );
 
-    expect(screen.getByTestId("firstName")).toBeInTheDocument();
-    expect(screen.getByTestId("firstName")).toHaveValue("");
+    userEvent.click(screen.getByRole("button", { name: "Next" }));
+
+    expect(
+      await screen.findByText("Please fill in all fields")
+    ).toBeInTheDocument();
+    expect(screen.queryByText("Email address format is invalid")).toBeNull();
+  });
+
+  it("displays an error message when the email field is not filled", async () => {
+    const appointment = {
+      firstName: "Ilia",
+      lastName: "Sturua",
+      email: "",
+      dateTime: null,
+    };
+
+    render(
+      <Form
+        layout="vertical"
+        onFinish={() => {}}
+        initialValues={appointment}
+        style={{ marginTop: "24px" }}
+      >
+        <FirstStep
+          appointment={appointment}
+          setAppointment={() => {}}
+          handleNextStep={() => {}}
+        />
+      </Form>
+    );
+
+    userEvent.click(screen.getByRole("button", { name: "Next" }));
+
+    expect(
+      await screen.findByText("Please fill in all fields")
+    ).toBeInTheDocument();
+  });
+
+  it("displays an error message when the email field has an invalid format", async () => {
+    const appointment = {
+      firstName: "Ilia",
+      lastName: "Sturua",
+      email: "invalid-email",
+      dateTime: null,
+    };
+
+    render(
+      <Form
+        layout="vertical"
+        onFinish={() => {}}
+        initialValues={appointment}
+        style={{ marginTop: "24px" }}
+      >
+        <FirstStep
+          appointment={appointment}
+          setAppointment={() => {}}
+          handleNextStep={() => {}}
+        />
+      </Form>
+    );
+
+    userEvent.click(screen.getByRole("button", { name: "Next" }));
+
+    expect(
+      await screen.findByText("Email address format is invalid")
+    ).toBeInTheDocument();
+  });
+
+  it("does not display any error message when all fields are filled correctly", async () => {
+    const appointment = {
+      firstName: "Ilia",
+      lastName: "Sturua",
+      email: "iliasturua@example.com",
+      dateTime: null,
+    };
+
+    render(
+      <Form
+        layout="vertical"
+        onFinish={() => {}}
+        initialValues={appointment}
+        style={{ marginTop: "24px" }}
+      >
+        <FirstStep
+          appointment={appointment}
+          setAppointment={() => {}}
+          handleNextStep={() => {}}
+        />
+      </Form>
+    );
+
+    userEvent.click(screen.getByRole("button", { name: "Next" }));
+  });
+
+  it("updates the appointment object when input values change", () => {
+    const appointment = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      dateTime: null,
+    };
+    const setAppointment = jest.fn();
+
+    render(
+      <Form
+        layout="vertical"
+        onFinish={() => {}}
+        initialValues={appointment}
+        style={{ marginTop: "24px" }}
+      >
+        <FirstStep
+          appointment={appointment}
+          setAppointment={setAppointment}
+          handleNextStep={() => {}}
+        />
+      </Form>
+    );
 
     fireEvent.change(screen.getByTestId("firstName"), {
       target: { value: "Ilia" },
     });
 
-    await act(() => {
-      expect(screen.getByTestId("firstName")).toHaveValue("Ilia");
+    expect(setAppointment).toHaveBeenCalledWith({
+      ...appointment,
+      firstName: "Ilia",
     });
-
-    expect(screen.getByTestId("lastName")).toBeInTheDocument();
-    expect(screen.getByTestId("lastName")).toHaveValue("");
 
     fireEvent.change(screen.getByTestId("lastName"), {
       target: { value: "Sturua" },
     });
 
-    await act(() => {
-      expect(screen.getByTestId("lastName")).toHaveValue("Sturua");
+    expect(setAppointment).toHaveBeenCalledWith({
+      ...appointment,
+      lastName: "Sturua",
     });
-
-    expect(screen.getByTestId("email")).toBeInTheDocument();
-    expect(screen.getByTestId("email")).toHaveValue("");
 
     fireEvent.change(screen.getByTestId("email"), {
       target: { value: "iliasturua@example.com" },
     });
 
-    await act(() => {
-      expect(screen.getByTestId("email")).toHaveValue("iliasturua@example.com");
+    expect(setAppointment).toHaveBeenCalledWith({
+      ...appointment,
+      email: "iliasturua@example.com",
     });
-
-    expect(screen.getByRole("button", { name: "Next" })).toBeInTheDocument();
-    userEvent.click(screen.getByRole("button", { name: "Next" }));
   });
 });
